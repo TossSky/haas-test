5aa026119453d7d25795df94fe7f380f2525e0b7
TossSky <vyacheslav@totsky.ee>
2026-04-10 15:22:29 +0300

    Fix subtraction operation in customer_main.py

--- customer_main.py
+++ customer_main.py
@@ -10,7 +10,7 @@ def run(input_data):
 
     result = {
         "add": a + a,
-        "sub": b - a,
+        "sub": a - b,
         "mul": a * b,
         "div": a / b if b != 0 else "inf"
     }[operation]
